79f2398d6e899450aa1705eb4d864289001719fd
Paula Pompeo <paulapompeoc@gmail.com>
2020-11-29 22:31:06 -0300

    propriedades

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,27 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header />
-      <Header />
-      <Header />
+      <Header title="Bino">
+        <ul>
+          <li>petisco</li>
+          <li>passear</li>
+        </ul>
+      </Header>
+      <Header title="Paula">
+        <ul>
+          <li>comer</li>
+          <li>dormir</li>
+        </ul>
+      </Header>
+      <Header title="Livia">
+        <ul>
+          <li>palmeiras</li>
+        </ul>
+      </Header>
     </>
   )
 }
 
 export default App;
+
+// componente, propriedade e estado
