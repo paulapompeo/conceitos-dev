f971d59d787db4e07a85db48a887670d4d488176
Paula Pompeo <paulapompeoc@gmail.com>
2020-11-30 00:16:32 -0300

    adicionar cors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpg';
+// import backgroundImage from './assets/background.jpg';
 
 import Header from './components/Header';
 
 function App() {
   const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      console.log(response)
+    })
+  }, [])
 
   function handleAddProject() {
     // projects.push(`Novo Projeto ${Date.now()}`); nao faz mais assim
@@ -20,7 +27,7 @@ function App() {
     <>
       <Header title="Projects" />
 
-      <img width={500} src={backgroundImage}/>
+      {/* <img width={500} src={backgroundImage}/> */}
 
       <ul>
         {projects.map(project => <li key={project}>{project}</li>)}
@@ -57,3 +64,8 @@ export default App;
 //useState retorna um array com 2 posicoes
 //1. Variavel com o seu valor inicial
 //2. Funcao para atualizarmos o valor
+
+//useEffect -> dispara funcoes sempre que tivermos alguma info alterada, ou quando componente
+//for exibido em tela
+//1. Qual funcao quero disparar
+//2. Quando quero disparar
