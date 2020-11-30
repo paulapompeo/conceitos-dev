b0d721c0e1e70ab029109a3cbaa20ecbc6aa7be8
Paula Pompeo <paulapompeoc@gmail.com>
2020-11-29 22:19:36 -0300

    componentizacao

--- frontend/public/script.js
+++
@@ -1,9 +0,0 @@
-// babel: transpilar (converter) o codigo do React para um codigo que browser entenda -> funcoes
-// webpack: Pra cada tipo de arquivo (js, css, png) eu vou converter o codigo de uma maneira diferente
-
-//loader: -> webpack usa
-
-// babel-loader: responsavel por converter o js para algo que o browser entenda
-// css-loader
-// image-loader
-
